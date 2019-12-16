909e4b0433c96e2c331b7f041f86a196b87f3759
j-top <58952323+j-top@users.noreply.github.com>
2019-12-16 14:08:09 -0500

    Same old

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.createTime = function(input="00:00"){
+module.exports.createTime = function createTime(input="00:00"){
     if(typeof input !== typeof "easter egg") throw new Error("Invalid Type: Must be of type 'string'.").stack;
     if(input.length !== 5) throw new Error("Invalid Length: Must be of length 5.").stack;
     var split = input.split(':');
@@ -13,19 +13,19 @@ module.exports.createTime = function(input="00:00"){
 
 const simpleTimeError = new Error("Invalid Type: Parameters must match simpleTime format: \nObject {\n  hours: Number,\n  minutes: Number\n}");
 
-module.exports.isSimpleTime = function(input){
+module.exports.isSimpleTime = function isSimpleTime(input){
     if(input.hours && input.minutes && !isNaN(input.hours) || input.hours === 0 && !isNaN(input.minutes)) return true;
     return false;
 }
 
-module.exports.strTime = function(t1){
+module.exports.strTime = function strTime(t1){
     if(isSimpleTime(t1)){
         return "" + (t1.hours < 10 ? "0" + t1.hours : t1.hours) + ":" + (t1.minutes < 10 ? "0" + t1.minutes : t1.minutes);
     }
     else throw simpleTimeError.stack;
 }
 
-module.exports.addTime = function(t1, t2, overwrite=false){
+module.exports.addTime = function addTime(t1, t2, overwrite=false){
     if(isSimpleTime(t1) && isSimpleTime(t2)){
         const temp = { hours: t1.hours, minutes: t1.minutes };
         temp.minutes += t2.minutes;
@@ -45,7 +45,7 @@ module.exports.addTime = function(t1, t2, overwrite=false){
     else throw simpleTimeError.stack;
 }
 
-module.exports.subTime = function(t1,t2, overwrite=false){
+module.exports.subTime = function subTime(t1,t2, overwrite=false){
     if(isSimpleTime(t1) && isSimpleTime(t2)){
         const temp = { hours: t1.hours, minutes: t1.minutes };
         temp.minutes -= t2.minutes;
@@ -65,7 +65,7 @@ module.exports.subTime = function(t1,t2, overwrite=false){
     else throw simpleTimeError.stack;
 }
 
-module.exports.difTime = function(t1,t2){
+module.exports.difTime = function difTime(t1,t2){
     if(isSimpleTime(t1) && isSimpleTime(t2)){
         const difTime = { hours: 0, minutes: 0 };
         var lessOneHour = 0;
